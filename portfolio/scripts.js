460c82c41fba835a37068d66e4d4dac6ff52d0f5
WeGoToMars <profigor7@gmail.com>
2021-11-07 15:22:54 +0200

    Added SAT Day time-lapse

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -58,3 +58,18 @@ const instance =
 instance.listen('.zoomable')
 instance.listen('.astropic img')
 instance.listen('.screenshot-container img')
+
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        console.log(content)
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+};
