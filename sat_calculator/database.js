96d6aa60c43d9a07176d3c228d56d92f2f75caa4
Ihor Makhynia <profigor7@gmail.com>
2022-09-09 18:50:40 -0400

    Added Aug 2022 curves

--- sat_calculator/database.js
+++ sat_calculator/database.js
@@ -27,7 +27,9 @@ var curvesDataReddit =
 'Mar 2022', //https://www.reddit.com/r/Sat/comments/tnir39/comment/i229nze
 'Mar 2022 International', //https://www.reddit.com/r/Sat/comments/tniv2o/comment/i21zssp
 'May 2022', //https://www.reddit.com/r/Sat/comments/utib78/comment/i9bedzf
-'May 2022 International'] //https://www.reddit.com/r/Sat/comments/utib7q/comment/i9beqwm
+'May 2022 International', //https://www.reddit.com/r/Sat/comments/utib7q/comment/i9beqwm
+'Aug 2022', //https://www.reddit.com/r/Sat/comments/x9slj7/comment/inptz1m
+'Aug 2022 International'] //https://www.reddit.com/r/Sat/comments/x9skir/comment/inpufle
 
 // "Reddit curves" data, only first 10 data points are selected for consistency
 var mathCurveReddit = [
@@ -54,7 +56,9 @@ var mathCurveReddit = [
 [800, 790, 780, 770, 760, 740, 730, 720, 710, 700],
 [800, 790, 780, 770, 760, 740, 730, 720, 710, 700],
 [800, 800, 790, 780, 760, 750, 740, 730, 720, 700],
-[800, 800, 790, 780, 760, 750, 740, 720, 710, 700]
+[800, 800, 790, 780, 760, 750, 740, 720, 710, 700],
+[800, 790, 780, 770, 760, 750, 730, 720, 710, 700],
+[800, 790, 780, 760, 750, 730, 720, 710, 700, 690]
 ];
 var readingCurveReddit = [
 [400, 390, 380, 370, 370, 360, 350, 340, 340, 330],
@@ -80,7 +84,9 @@ var readingCurveReddit = [
 [400, 400, 390, 380, 370, 370, 360, 350, 350, 340],
 [400, 400, 390, 390, 380, 370, 370, 360, 350, 350],
 [400, 400, 390, 390, 390, 380, 380, 370, 360, 360],
-[400, 400, 390, 380, 370, 350, 350, 350, 340, 340]
+[400, 400, 390, 380, 370, 350, 350, 350, 340, 340],
+[400, 400, 400, 390, 380, 380, 370, 360, 350, 350],
+[400, 400, 390, 390, 380, 370, 370, 360, 350, 340]
 ];
 var writingCurveReddit = [
 [400, 390, 370, 360, 350, 340, 330, 320, 310, 310],
@@ -106,7 +112,9 @@ var writingCurveReddit = [
 [400, 390, 380, 370, 360, 350, 340, 330, 330, 320],
 [400, 390, 390, 370, 360, 350, 340, 340, 330, 320],
 [400, 390, 390, 380, 360, 350, 350, 330, 330, 320],
-[400, 390, 390, 370, 360, 350, 350, 340, 340, 330]
+[400, 390, 390, 370, 360, 350, 350, 340, 340, 330],
+[400, 390, 380, 370, 360, 350, 340, 330, 330, 320],
+[400, 390, 380, 360, 350, 350, 340, 330, 330, 320]
 ];
 
 // Full curves data
